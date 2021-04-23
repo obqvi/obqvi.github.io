ed36e4a835b5448514b23d5d4fbd15e666fdf9ef
krisikostadinov <krisi@DESKTOP-K83LGMJ.(none)>
2021-04-23 12:03:35 +0300

    Add design of ligin and register.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './Login.css';
 
 export const Login = () => {
     return (
-        <div>
+        <div className="form">
             <title>Вход</title>
+            <h2>Вход</h2>
+            <form>
+                <div className="form-control">
+                    <label>Имейл</label>
+                    <input type="email" name="email" />
+                </div>
+                <div className="form-control">
+                    <label>Парола</label>
+                    <input type="password" name="password" />
+                </div>
+                <div className="btn-group">
+                    <button type="submit" className="btn primary">Вход</button>
+                </div>
+            </form>
         </div>
     )
 }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React from 'react';
+import './Register.css';
 
 export const Register = () => {
     return (
-        <div>
+        <div className="form">
             <title>Регистрация</title>
             <h2>Регистрация</h2>
+            <form>
+                <div className="form-control">
+                    <label>Име</label>
+                    <input type="text" name="firstName" />
+                </div>
+                <div className="form-control">
+                    <label>Фамилия</label>
+                    <input type="text" name="lastName" />
+                </div>
+                <div className="form-control">
+                    <label>Имейл</label>
+                    <input type="email" name="email" />
+                </div>
+                <div className="form-control">
+                    <label>Парола</label>
+                    <input type="password" name="password" />
+                </div>
+                <div className="form-control">
+                    <label>Потвърди паролата</label>
+                    <input type="password" name="confirmPassword" />
+                </div>
+                <div className="btn-group">
+                    <button type="submit" className="btn primary">Регистрация</button>
+                </div>
+            </form>
         </div>
     )
 }
